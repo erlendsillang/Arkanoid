92b557780bce3fec73e60e4c046cb893c62f01b4
Erlend Sillang <erlend.sillang@ivkhk.ee>
2025-11-21 19:48:08 +0200

    suuname palli ekraanile

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 let game = {
     ctx: null,
+    platform:null,
+    ball: null,
     sprites: {
 background: null,
 ball: null,
@@ -30,9 +32,10 @@ platform: null
     },
 render() {
     this.ctx.drawImage(this.sprites.background, 0, 0);
-    this.ctx.drawImage(this.sprites.ball, 0, 0);
-    this.ctx.drawImage(this.spites.platform, 0, 0);
-},
+    this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
+        this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+    this.ctx.drawImage(this.spites.platform, this.platform.x, this.platform.y);
+    },
 start:function() {
     this.init();
     this.preload(() => {
@@ -40,6 +43,19 @@ start:function() {
     });
 }
 };
+
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+
+game.platform = {
+    x: 280,
+    y: 300
+};
+
 window.addEventListener("load", () => {
     game.start();
 });
