d129bf23365700a5f9d3c5a06343e6f17aea76b2
Erlend Sillang <erlend.sillang@ivkhk.ee>
2025-12-01 19:12:25 +0200

    Kuvame blokid ekraanil

--- game.js
+++ game.js
@@ -2,10 +2,14 @@ let game = {
     ctx: null,
     platform:null,
     ball: null,
-    sprites: {
-background: null,
+    blocks: [],
+    rows: 4,
+    cols: 8,
+    sprites: { 
+        background: null,
 ball: null,
-platform: null
+platform: null,
+block: null
     },
     init: function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
@@ -13,19 +17,30 @@ platform: null
     preload(callback) {
         let loaded = 0;
         let required = Object.keys(this.sprites).length;
+        let onImageLoad = () => {
+            ++loaded;
+            if (loaded >= required) {
+                callback() ;
+                }
+                };
 
         for (let key  in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = "img/" + key + ".png";
-            this.sprites[key].addEventListener("load",() => {
-                ++loaded;
-                if (loaded >= required) {
-                    callback();
+            this.sprites[key].addEventListener("load", onImageLoad);
                 }
-            });
-        }
-    },
-        run() { 
+         },
+                create() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+                this.blocks.push({
+                    x: 64 * col + 65,
+                    y: 24 * row + 35
+            });              
+                }
+            }
+        },
+    run() { 
         window.requestAnimationFrame(() => {
             this.render();
         });
@@ -35,28 +50,34 @@ render() {
     this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
         this.ball.x, this.ball.y, this.ball.width, this.ball.height);
     this.ctx.drawImage(this.spites.platform, this.platform.x, this.platform.y);
-    },
-start:function() {
+    this.renderBlocks();
+},
+    renderBlocks() {
+        for  (let block of this.blocks) {
+             this .ctx.drawImage(this.sprites.block, block.x, block.y);
+    }
+},
+start: function() {
     this.init();
     this.preload(() => {
-        this.run();
+    this.create();
+    this.run();
     });
 }
 };
 
-game.ball = {
+game.platform = {
     x: 320,
     y: 280,
-    width: 20,
-    height: 20
+width: 20,
+height: 20
 };
 
 game.platform = {
     x: 280,
-    y: 300
+    y: 300,
 };
 
-window.addEventListener("load", () => {
+window.addEventListener ("load", () => {
     game.start();
-});
-
+    });
